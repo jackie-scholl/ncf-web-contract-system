31a75ef8022f587bbf565ab256eb208ea224339e
Jackie Scholl <jackie.h.scholl@gmail.com>
2016-05-01 17:17:31 -0400

    initial implementation of contract list view

--- src/main/resources/static/js/contract_list.js
+++ src/main/resources/static/js/contract_list.js
@@ -4,7 +4,52 @@ onSignInExtra = function() {
 		{id_token: googleUser.getAuthResponse().id_token},
 		function(data) {
 			console.log(data);
-			alert("First contract id: " + data.contracts[0].contractId);
+			$("#content").append(data.contracts[0].contractId);
+			var items = [];
+			  $.each(data.contracts, function(_, entry ) {
+			    items.push( "<li id='" + entry.contractId + "'>" + link(entry) + "</li>" );
+			  });
+			 
+			  $( "<ul/>", {
+			    "class": "my-new-list",
+			    html: items.join("")
+			  }).appendTo("#content");
 		}
 	);
+}
+
+if (googleUser != null) {
+	onSignInExtra();
+}
+
+function link(entry) {
+	return "<a href='/contracts/"+entry.contractId+"'>" +timeSince(new Date(entry.dateLastModified)) + " ago </a>";
+}
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
 }

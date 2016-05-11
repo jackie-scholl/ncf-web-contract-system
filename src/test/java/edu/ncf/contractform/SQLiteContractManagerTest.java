19d3c642645521566befa564706541c926cd0f71
Jackie Scholl <jackie.h.scholl@gmail.com>
2016-05-11 13:45:19 -0400

    added tests for sqlite contract manager

---
+++ src/test/java/edu/ncf/contractform/SQLiteContractManagerTest.java
@@ -0,0 +1,46 @@
+package edu.ncf.contractform;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SQLiteContractManagerTest {
+	private SQLiteContractManager instance;
+	private final static String testDatabaseUrl = "ContractsTestDatabase.db";
+	
+	@Before
+	public void setUp() throws IOException {
+		deleteDatabaseFile();
+		this.instance = SQLiteContractManager.getTest(testDatabaseUrl);
+	}
+	
+	private static void deleteDatabaseFile() throws IOException {
+		try {
+		    Files.delete(FileSystems.getDefault().getPath(testDatabaseUrl));
+		} catch (NoSuchFileException x) {}
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		deleteDatabaseFile();
+	}
+
+	@Test
+	public void testGetAllContracts() {
+		assertNotNull(instance.getAllContracts());
+	}
+	
+	@Test
+	public void testTableIsEmpty() {
+		assertEquals(instance.getAllContracts().size(), 0);
+	}
+
+}

51479dbb8aeb3fda1b7f0684716f24dd42d53419
Jackie Scholl <jackie.h.scholl@gmail.com>
2016-06-07 14:15:52 -0400

    Add working LambdaHandler

--- src/main/java/edu/ncf/contractform/LambdaHandler.java
+++ src/main/java/edu/ncf/contractform/LambdaHandler.java
@@ -1,14 +1,25 @@
 package edu.ncf.contractform;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+
 import com.amazonaws.services.lambda.runtime.Context; 
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.google.gson.Gson;
+//import com.amazonaws.services.lambda.runtime.LambdaLogger;
+//import com.google.gson.Gson;
 import org.apache.commons.io.IOUtils;
 
 public class LambdaHandler {
+	private static boolean REQUIRE_GOOGLE = false;
 	public void handler(InputStream inputStream, OutputStream outputStream, Context context) {
-		//String contractRequestJson = IOUtils.toString(inputStream, "UTF-8");
+		try {
+			String contractRequestJson = IOUtils.toString(inputStream, "UTF-8");
+			RenderContractRequest.renderContractPDF(contractRequestJson, outputStream, REQUIRE_GOOGLE);
+		} catch (IOException e) {
+			context.getLogger().log(Arrays.toString(e.getStackTrace()));
+			throw new RuntimeException(e);
+		}
+		
 	}
 }

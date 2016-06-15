d215748cf9ab3a63e8dd56ff0bb112367ac4adb0
Jackie Scholl <jackie.h.scholl@gmail.com>
2016-06-15 14:04:08 -0400

    Temporarily change sign-out process
    For ease of testing, this commit stops the system from revoking
    credentials, so when you sign out it’s easier to sign back in

--- src/main/resources/js/login.js
+++ src/main/resources/js/login.js
@@ -92,7 +92,7 @@ const GoogleLoginArea = React.createClass({
   signOut: function() {
     console.log('Signing out ' + this.state.googleUser.getBasicProfile().getName());
     const auth2 = gapi.auth2.getAuthInstance();
-    auth2.disconnect();
+    //auth2.disconnect();
     auth2.signOut().then(function() {
       console.log('User signed out.');
       this.updateLogin(null);

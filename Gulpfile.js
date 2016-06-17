785cf1db59bd535510f2c6bf7f73bb0f0d247e67
Jackie Scholl <jackie.h.scholl@gmail.com>
2016-06-17 16:05:16 -0400

    Properly lints gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('lint-scripts', function () {
   // So, it's best to have gulp ignore the directory as well.
   // Also, Be sure to return the stream from the task;
   // Otherwise, the task may end before the stream has finished.
-  return gulp.src([paths2.src.scripts, '.Gulpfile.js'])
+  return gulp.src([paths2.src.scripts, 'Gulpfile.js'])
     // eslint() attaches the lint output to the 'eslint' property
     // of the file object so it can be used by other modules.
     .pipe(eslint())

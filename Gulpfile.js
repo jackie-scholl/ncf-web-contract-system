2fa40a1929f75d9dad35539a76cb89153dd6f8d3
Jackie Scholl <jackie.h.scholl@gmail.com>
2016-06-17 13:29:38 -0400

    Adds linting to project

--- Gulpfile.js
+++ Gulpfile.js
@@ -13,6 +13,7 @@ const uglify = require('gulp-uglify');
 const buffer = require('vinyl-buffer');
 const watchify = require('watchify');
 const source = require('vinyl-source-stream');
+const eslint = require('gulp-eslint');
 
 const resources = 'src/main/resources/';
 const paths = {
@@ -79,6 +80,23 @@ gulp.task('clean-scripts', function() {
     .pipe(gulp.dest(target+'js'));
 });*/
 
+gulp.task('lint-scripts', function () {
+  // ESLint ignores files with 'node_modules' paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src([paths2.src.scripts, '.Gulpfile.js'])
+    // eslint() attaches the lint output to the 'eslint' property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
+});
+
 // Based on https://gist.github.com/danharper/3ca2273125f500429945
 function compile(watch) {
   //const js_src = resources+'js/main.js';
@@ -113,7 +131,7 @@ function watch() {
   return compile(true);
 };
 
-gulp.task('scripts', ['clean-scripts'], function() { return compile(); });
+gulp.task('scripts', ['clean-scripts', 'lint-scripts'], function() { return compile(); });
 gulp.task('watch-scripts', function() { return watch(); });
 
 gulp.task('clean-html', function() {
